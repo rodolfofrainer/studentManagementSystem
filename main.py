bfb3af89bf837de69e1cfa0cfdd9b7e9129859e4
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-03-27 17:52:12 +0100

    add columns for future data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
 from PyQt6.QtGui import QAction
 import sys
 
@@ -16,6 +16,15 @@ class MainWindow(QMainWindow):
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(
+            ('Id', 'Name', 'Course', 'Mobile'))
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        pass
 
 
 app = QApplication(sys.argv)

a996f40e41b4b4cf5a87566bd311b1e3cdb60793
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-03-28 14:41:58 +0100

    set window minimum size

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Student Management System')
+        self.setMinimumSize(800, 600)
 
         file_menu_item = self.menuBar().addMenu('&File')
         help_menu_item = self.menuBar().addMenu('&Help')
